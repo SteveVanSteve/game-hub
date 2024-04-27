17f0b054dcac4dc0c8d528139f7191de7394b7cf
Steve <stevie.shields@hotmail.co.uk>
2024-04-27 17:10:09 +0100

    Implemented 'dark' mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,19 @@ import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <Grid templateAreas={{
-      base: `"nav" "main"`,
-      lg: `"nav nav" "aside main"`
-    }}>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" bg="gold">
-        Aside
-      </GridItem>
+          Aside
+        </GridItem>
       </Show>
       <GridItem area="main" bg="dodgerblue">
         Main
